37a34d04c37a43a054474df76a39a36075dbcc13
BlazerXXI <ruslandendik@gmail.com>
2023-07-20 03:03:07 +0300

    dev 20.07

--- js/index.js
+++ js/index.js
@@ -3,58 +3,76 @@
 const weatherBlock = document.querySelector("#weather");
 
 async function loadWeather() {
-  weatherBlock.innerHTML = `
+	weatherBlock.innerHTML = `
 	<div class="weather__loading">
 	<img src="../images/loading-gif.gif" alt="loading..." />
 	</div>
 	`;
 
-  const cityInput = document.querySelector("#cityInput");
-  const apiKey = "573b5b9bc37c7923940895f646152395";
-  const city = "Kharkiv";
+	const cityInput = document.querySelector("#cityInput");
+	const apiKey = "573b5b9bc37c7923940895f646152395";
+	const city = cityInput.value;
 
-  cityInput.addEventListener("input", () => {
-    const newCity = cityInput.value;
-    loadWeather(newCity);
-  });
+	cityInput.addEventListener("input", () => {
+		const newCity = cityInput.value;
+		city = newCity;
+		loadWeather();
+		if (newCity.trim() === "") {
+			return;
+		}
+	});
+	cityInput.addEventListener("keypress", (event) => {
+		if (event.key === "Enter") {
+			loadWeather();
+		}
+	});
 
-  const server = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+	const server = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
 
-  const response = await fetch(server, {
-    method: "GET",
-  });
-  const responseResult = await response.json();
+	const response = await fetch(server, {
+		method: "GET",
+	});
+	const responseResult = await response.json();
 
-  response.ok
-    ? getWeather(responseResult)
-    : (weatherBlock.innerHTML = responseResult.message);
+	response.ok
+		? getWeather(responseResult)
+		: (weatherBlock.innerHTML = "Введіть назву міста");
 }
 
 getWeather = (data) => {
-  console.log(data);
+	console.log(data);
 
-  const location = data.name;
-  const temp = Math.round(data.main.temp);
-  const feelsLike = Math.round(data.main.feels_like);
-  const weatherStatus = data.weather[0].main;
-  const weatherIcon = data.weather[0].icon;
+	const location = data.name;
+	const temp = Math.round(data.main.temp);
+	const feelsLike = Math.round(data.main.feels_like);
+	const weatherStatus = data.weather[0].main;
+	const weatherIcon = data.weather[0].icon;
 
-  const template = `
+	const template = `
 	<div class="weather__header">
 	<div class="weather__main">
 	<div class="weather__city">${location}</div>
 	<div class="weather__status">${weatherStatus}</div>
-	</div>
 	<div class="weather__icon">
+	</div>
 	<img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}" />
 	</div>
 	</div>
 	<div class="weather__temp">${temp}</div>
 	<div class="weather__feels-like feels__temp">Feels like: ${feelsLike}</div>`;
 
-  weatherBlock.innerHTML = template;
+	weatherBlock.innerHTML = template;
 };
 
 if (weatherBlock) {
-  loadWeather();
+	loadWeather();
 }
+
+const submitBtn = document.querySelector("#submitBtn");
+
+submitBtn.addEventListener("click", () => {
+	const city = cityInput.value.trim();
+	if (city !== "") {
+		loadWeather(city);
+	}
+});
